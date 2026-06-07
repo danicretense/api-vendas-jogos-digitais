df0d9d6deaca3c909ee754179cbb8a648f751a4d
Daniela <danicretense@gmail.com>
2026-06-07 01:47:13 -0300

    vai na fé

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,23 +1,45 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
+
+  console.log("HEADERS RECEBIDOS:");
+  console.log(req.headers);
+
   const authHeader = req.headers.authorization;
 
+  console.log("AUTH HEADER:", authHeader);
+
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Acesso negado. Nenhum token fornecido.' });
+    return res.status(401).json({
+      message: 'Acesso negado. Nenhum token fornecido.'
+    });
   }
 
   const token = authHeader.split(' ')[1];
 
   try {
-     
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Adiciona o payload do token (ex: { id: 1, perfil: 'Cliente' }) ao request
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+
+    req.user = decoded;
+
     next();
+
   } catch (error) {
-     
-    res.status(401).json({ message: 'Token inválido.' });
+
+    console.log("ERRO JWT:", error.message);
+
+    return res.status(401).json({
+      message: 'Token inválido.'
+    });
   }
 };
 
 module.exports = authMiddleware;
+
+
+//
+
